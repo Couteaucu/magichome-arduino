f3afc7e64397449d2b7ed6c4890655f44ba5a1fc
Couteaucu <couteaucu@gmail.com>
2020-08-02 00:12:42 -0500

    barebones lightcontroller py file to accept arguments from command line

---
+++ LightController.py
@@ -0,0 +1,9 @@
+from magichome import MagicHomeApi
+
+def process_operation(operation):
+    roomled = MagicHomeApi('IP address', 0)
+    backlight = MagicHomeApi('IP address', 0)
+    mainlamp = MagicHomeApi('IP address', 3)
+    secondlamp = MagicHomeApi('IP address', 3)
+
+
