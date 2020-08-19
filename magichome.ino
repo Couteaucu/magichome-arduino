7374ba1297d87b6a4cf0356324f2c97705fa65e0
Couteaucu <couteaucu@gmail.com>
2020-08-19 13:51:49 -0500

    sync

--- magichome.ino
+++ magichome.ino
@@ -67,4 +67,105 @@ class MagicHomeAPI{
             return device.socket.recv(14);
         }
     }
+
+    void update_device(MagicHomeAPI device, int r = 0, int g = 0, int b = 0, int white1=NULL, int white2=NULL){
+        /*Updates a device based upon what we're sending to it.
+        Values are excepted as integers between 0-255.
+        Whites can have a value of None.*/
+        if(device.type <= 1){
+            white1 = device.check_number_range(white1);
+            int message[] = {0x31, r, g, b, white1, 0x00, 0x0f};
+            device.send_bytes(*(message+[device.calculate_checksum(message)]));
+        }else if(device.type == 2){
+            //Update an RGB + WW + CW device
+            int message[] = {0x31,
+                       device.check_number_range(r),
+                       device.check_number_range(g),
+                       device.check_number_range(b),
+                       device.check_number_range(white1),
+                       device.check_number_range(white2),
+                       0x0f, 0x0f};
+            device.send_bytes(*(message+[device.calculate_checksum(message)]));
+        } else if (device.type == 3){
+            //Update the white, or color, of a bulb
+            if (white1 != NULL){
+                int message[] = {0x31, 0x00, 0x00, 0x00,
+                           device.check_number_range(white1),
+                           0x0f, 0x0f};
+                device.send_bytes(*(message+[device.calculate_checksum(message)]))
+            } else{
+                int message[] = {0x31,
+                           device.check_number_range(r),
+                           device.check_number_range(g),
+                           device.check_number_range(b),
+                           0x00, 0xf0, 0x0f};
+                device.send_bytes(*(message+[device.calculate_checksum(message)]));
+            }
+        } else if (device.type == 4){
+             //Update the white, or color, of a legacy bulb
+           if (white1 != NULL){
+                int message[] = {0x56, 0x00, 0x00, 0x00,
+                           device.check_number_range(white1),
+                           0x0f, 0xaa, 0x56, 0x00, 0x00, 0x00,
+                           device.check_number_range(white1),
+                           0x0f, 0xaa};
+                device.send_bytes(*(message+[device.calculate_checksum(message)]));
+           } else{
+                int message[] = {0x56,
+                           device.check_number_range(r),
+                           device.check_number_range(g),
+                           device.check_number_range(b),
+                           0x00, 0xf0, 0xaa};
+                device.send_bytes(*(message+[device.calculate_checksum(message)]));
+                }
+        }else{
+            Serial.println("Incompatible device type received...");
+        }
+    }
+
+    //Check if the given number is in the allowed range.
+    int check_number_range(MagicHomeAPI device, int number){
+        if (number < 0){
+            return 0;
+        }else if (number > 255){
+            return 255;
+        }else{
+            return number;
+        }
+    }
+
+    //Send a preset command to a device.   
+    void send_preset_function(MagicHomeAPI device, int preset_number, int speed){
+        //Presets can range from 0x25 (int 37) to 0x38 (int 56)
+        if(preset_number < 37){
+            preset_number = 37;
+        }
+        else if(preset_number > 56){
+            preset_number = 56;
+        }
+
+        if (speed < 0){
+            speed = 0;
+        }else if (speed > 100){
+            speed = 100;
+        }
+
+        if(device.type == 4){
+            device.send_bytes(0xBB, preset_number, speed, 0x44);
+        }else{
+            int message[] = {0x61, preset_number, speed, 0x0F};
+            device.send_bytes(*(message+[device.calculate_checksum(message)]));
+        }
+    }
+
+    int calculate_checksum(MagicHomeAPI device, int* bytes){
+        //Calculate the checksum from an array of bytes.
+        int sum = 0;
+        for(int i = 0; i < sizeof(bytes); i++){
+            sum += bytes[i];
+        }
+        return sum & 0xFF;
+    }
+
+    void send_bytes(MagicHomeAPI device, )
 };
