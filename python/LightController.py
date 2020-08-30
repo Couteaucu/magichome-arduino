ef7454549c07f72ffc106e5447ec1c5e63055490
Couteaucu <couteaucu@gmail.com>
2020-08-30 16:01:26 -0500

    final commit - switching to new repository trying to upload python to arduino

--- python/LightController.py
+++ python/LightController.py
@@ -1,9 +1,20 @@
 from magichome import MagicHomeApi
 
-def process_operation(operation):
-    roomled = MagicHomeApi('IP address', 0)
-    backlight = MagicHomeApi('IP address', 0)
-    mainlamp = MagicHomeApi('IP address', 3)
-    secondlamp = MagicHomeApi('IP address', 3)
+roomled = MagicHomeApi('192.168.8.11', 0)
+backlight = MagicHomeApi('192.168.8.10', 0)
+mainlamp = MagicHomeApi('192.168.8.12', 3)
+#secondlamp = MagicHomeApi('192.168.8.13', 3)
 
+def allOn():
+    backlight.turn_on()
+    roomled.turn_on()
+    mainlamp.turn_on()
 
+def allOff():
+    backlight.turn_off()
+    roomled.turn_off()
+    mainlamp.turn_off()
+
+allOn()
+
+backlight.get
